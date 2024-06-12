7814fb8070d6ff0f4a81a9ed6034b279c03f3bdc
BlueCP <47088138+BlueCP@users.noreply.github.com>
2024-06-12 15:38:29 +0100

    Implement DeepCache

---
+++ test.py
@@ -0,0 +1,16 @@
+from LoRA_Cache.pipelines.stable_diffusion.pipeline_stable_diffusion import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5')
+# Without DeepCache
+image = pipe(
+    prompt='A girl looking up at the stars'
+).images[0]
+image.save('image.png', 'PNG')
+# With DeepCache
+image = pipe(
+    prompt='A girl looking up at the stars',
+    cache_interval=5,
+    cache_layer_id=0,
+    cache_block_id=1
+).images[0]
+image.save('image_dc.png', 'PNG')
